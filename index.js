3297e200d14463580723aa8ef5ff05b5bb847f5a
Mason534 <masonmiller534@gmail.com>
2021-09-17 03:05:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const dotenv = require('dotenv')
+const { Intents } = Discord;
+dotenv.config()
+
+const client = new Discord.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('the bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong'
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'how do i join') {
+        message.reply({
+            content: 'follow the how to join channel'
+        })
+    }
+})
+
+
+client.login(process.env.TOKEN)
