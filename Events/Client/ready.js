ab2f82516dd0d864668ccdca960d2b501f573b8e
Mason534 <masonmiller534@gmail.com>
2021-10-05 23:14:55 -0500

    Update 45

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,7 +1,20 @@
+const mongoose = require("mongoose");
+const { Database } = require("../../config.json");
+
 module.exports = {
     name: "ready",
     execute(client) {
         console.log('The bot is now online!');
         client.user.setActivity('for ~help', {type: 'WATCHING'});
+
+        if(!Database) return;
+        mongoose.connect(Database, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log("The bot is now connected to the Database!")
+        }).catch((Error) => {
+            console.log(Error)
+        });
     }
 };
