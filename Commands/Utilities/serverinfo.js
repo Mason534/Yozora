0e049c5d9c7bb85a623aaca244d8df39747a32a5
Mason534 <masonmiller534@gmail.com>
2021-10-09 14:27:19 -0500

    Create serverinfo.js

---
+++ Commands/Utilities/serverinfo.js
@@ -0,0 +1,128 @@
+const { MessageEmbed } = require('discord.js');
+
+const moment = require('moment');
+
+const filterLevels = {
+
+	DISABLED: 'Off',
+
+	MEMBERS_WITHOUT_ROLES: 'No Role',
+
+	ALL_MEMBERS: 'Everyone'
+
+};
+
+const verificationLevels = {
+
+	NONE: 'None',
+
+	LOW: 'Low',
+
+	MEDIUM: 'Medium',
+
+	HIGH: '(╯°□°）╯︵ ┻━┻',
+
+	VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
+
+};
+
+const regions = {
+
+	brazil: 'Brazil',
+
+	europe: 'Europe',
+
+	hongkong: 'Hong Kong',
+
+	india: 'India',
+
+	japan: 'Japan',
+
+	russia: 'Russia',
+
+	singapore: 'Singapore',
+
+	southafrica: 'South Africa',
+
+	sydeny: 'Sydeny',
+
+	'us-central': 'US Central',
+
+	'us-east': 'US East',
+
+	'us-west': 'US West',
+
+	'us-south': 'US South'
+
+};
+
+module.exports = {
+    name: 'serverinfo',
+    aliases: [''],
+    permissions: 'SEND_MESSAGES',
+    description: "Shows the server information",
+    cooldown: 10,
+    execute(message, args, commandName, client, Discord) {
+        
+        const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
+
+		const members = message.guild.members.cache;
+
+		const channels = message.guild.channels.cache;
+
+		const emojis = message.guild.emojis.cache;
+
+        const Response = new MessageEmbed()
+        .setColor('FF6F52')
+	.setTitle('Server Info')
+	.setAuthor(message.author.username, message.author.avatarURL({dynamic: true}))
+	.setDescription(`**Guild information for __${message.guild.name}__**`)
+	.setThumbnail(client.user.avatarURL({dynamic: true }))
+	.setFooter(`Solaris Bot was buzzed in by ${message.author.username}`)
+	.setTimestamp(message.createdTimestamp)
+    .addFields([{
+        name: 'Server name',
+        value: `${message.guild.name}`,
+        inline: true
+    }, {
+        name: 'Server Created',
+        value: `${moment(message.guild.createdTimestamp).format('LL')} ${moment(message.guild.createdTimestamp).format('LT')}  `,
+        inline: true
+    }, {
+        name: 'Verification Level',
+        value: `${message.guild.verificationLevel}`,
+        inline: true
+    }, {
+        name: 'Roles',
+        value: `${roles.length}`,
+        inline: true
+    },{
+        name: 'Members',
+        value: `${message.guild.memberCount}`,
+        inline: true
+    }, {
+        name: 'Bots',
+        value: `${members.filter(member => member.user.bot).size}`,
+        inline: true
+    }, {
+        name: 'Emojis',
+        value: `${emojis.size}`,
+        inline: true
+    }, {
+        name: 'Text Channels',
+        value: `${channels.filter(channel => channel.type === 'text').size}`,
+        inline: true
+    }, {
+        name: 'Voice Channels',
+        value: `${channels.filter(channel => channel.type === 'voice').size}`,
+        inline: true
+    }, {
+        name: 'Boosts',
+        value: `${message.guild.premiumSubscriptionCount || '0'}`,
+        inline: true
+    
+    }]);
+
+        message.channel.send({embeds: [Response]})
+    }
+}
