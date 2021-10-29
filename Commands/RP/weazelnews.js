8ee6fcf16df9faeae2f51d3bf37cecd7537cfcba
Mason534 <masonmiller534@gmail.com>
2021-10-29 08:12:20 -0500

    Update weazelnews.js

--- Commands/RP/weazelnews.js
+++ Commands/RP/weazelnews.js
@@ -1,8 +1,8 @@
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name: 'weazel',
-    aliases: ['write'],
+    name: 'weazelarticle',
+    aliases: [''],
     permissions: 'SEND_MESSAGES',
     description: "posts a news embed",
     cooldown: 3,
