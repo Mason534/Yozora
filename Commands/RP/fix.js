b03bb84ea63da441f5f1ed7de38f72341bcf8c3d
Mason534 <masonmiller534@gmail.com>
2022-03-30 20:09:19 -0500

    Create fix.js

---
+++ Commands/RP/fix.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: 'fix',
+    aliases: [''],
+    permissions: 'SEND_MESSAGES',
+    description: "fixes car",
+    cooldown: 3,
+    execute(message, args, commandName, client, Discord) {
+        const Target = message.author;
+
+        message.reply("Please wait 20 seconds for your car to be repaired")
+        .then((msg) => {
+            setTimeout(function() {
+            message.channel.send(`${Target} Your car has been repaired, watch out next time!`);
+          }, 20000)
+        })
+    }
+}
