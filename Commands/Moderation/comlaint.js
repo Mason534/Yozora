cab38fa9fbf93318eb844b027258d5005be173cf
Mason534 <masonmiller534@gmail.com>
2022-04-03 01:15:18 -0500

    Create comlaint.js

---
+++ Commands/Moderation/comlaint.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+
+module.exports = {
+    name: 'complaint',
+    description: 'Sends any complaint someone wrote directly into staff chat.',
+    permission: "SEND_MESSAGES",
+
+    async execute(message, args, client, member, guild){
+
+        const channel = message.guild.channels.cache.get('559349489840947250'); 
+
+        let suggestmessage = args.slice().join(' ')
+        setTimeout(function() {
+            message.delete()
+          }, 100)
+
+        let suggestembed = new Discord.MessageEmbed()
+        .setTitle('A new complaint has been filed!:')
+        .setDescription(`${suggestmessage}`)
+        .setTimestamp()
+        .setColor("FF6F52")
+    }
+};
