08bc70f6cec3441a08d05e11e2a0d89a8c6bfdf4
Mason534 <masonmiller534@gmail.com>
2022-04-03 06:06:24 -0500

    Create deposit.js

---
+++ Commands/databaseCommands/deposit.js
@@ -0,0 +1,30 @@
+const profileModel = require("../../Models/profileSchema");
+
+module.exports = {
+  name: "deposit",
+  aliases: ["dep"],
+  permissions: [],
+  description: "Deposit coins into your bank!",
+  async execute(message, args, cmd, client, discord, profileData) {
+    const amount = args[0];
+    if (amount % 1 != 0 || amount <= 0) return message.channel.send("__Deposit amount must be a whole number__");
+    try {
+      if (amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to deposit`);
+      await profileModel.findOneAndUpdate(
+        {
+          userID: message.author.id,
+        },
+        {
+          $inc: {
+            coins: -amount,
+            bank: amount,
+          },
+        }
+      );
+
+      return message.channel.send(`You deposited **${amount}** into your bank`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
