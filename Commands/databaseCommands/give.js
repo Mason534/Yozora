685add6182c20511299b2c05e28fb43808851169
Mason534 <masonmiller534@gmail.com>
2022-04-03 06:45:55 -0500

    v

---
+++ Commands/databaseCommands/give.js
@@ -0,0 +1,44 @@
+const profileModel = require("../../Models/profileSchema");
+module.exports = {
+  name: "give",
+  aliases: [],
+  permissions: [],
+  description: "give a player some cash",
+  async execute(message, args, cmd, client, discord, profileData) {
+    const role = message.guild.roles.cache.find(r => r.id === "565275714035646475");
+
+        const User = message.author;
+        if(!message.member.roles.cache.has('565275714035646475'))
+            return (message.reply(`You're not allowed to use this command!`));
+
+            else
+            !message.member.roles.cache.has(role) 
+
+    if (!args.length) return message.channel.send("You need to mention a player to give them coins");
+    const amount = args[1];
+    const target = message.mentions.users.first();
+    if (!target) return message.channel.send("That user does not exist");
+
+    
+
+    try {
+      const targetData = await profileModel.findOne({ userID: target.id });
+      if (!targetData) return message.channel.send(`This user doens't exist in the db`);
+
+      await profileModel.findOneAndUpdate(
+        {
+          userID: target.id,
+        },
+        {
+          $inc: {
+            coins: amount,
+          },
+        }
+      );
+
+      return message.channel.send(`This player has been given ${amount} of cash!`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
---
+++ Commands/databaseCommands/withdraw.js
@@ -0,0 +1,32 @@
+const profileModel = require("../../Models/profileSchema");
+
+module.exports = {
+  name: "withdraw",
+  aliases: ["wd"],
+  permissions: [],
+  description: "withdraw coins from your bank",
+  async execute(message, args, cmd, client, discord, profileData) {
+    const amount = args[0];
+    if (amount % 1 != 0 || amount <= 0) return message.channel.send("__Withdraw amount must be a whole number!__");
+
+    try {
+      if (amount > profileData.bank) return message.channel.send(`You don't have that amount of cash to withdraw!`);
+
+      await profileModel.findOneAndUpdate(
+        {
+          userID: message.author.id,
+        },
+        {
+          $inc: {
+            coins: amount,
+            bank: -amount,
+          },
+        }
+      );
+
+      return message.channel.send(`You withdrew ${amount} of cash into your wallet!`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
