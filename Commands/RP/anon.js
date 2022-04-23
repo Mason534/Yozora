d6035b70be1c9713e5f05a5c4105263aac525c4e
Mason534 <masonmiller534@gmail.com>
2022-04-23 01:09:20 -0500

    Create anon.js

---
+++ Commands/RP/anon.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+
+module.exports = {
+    name: 'anon',
+    description: 'anon command',
+    permission: "SEND_MESSAGES",
+
+    async execute(message, args, client, member, guild){
+
+        const channel = message.guild.channels.cache.get('700694988404817940'); 
+
+        let suggestmessage = args.slice().join(' ')
+        setTimeout(function() {
+            message.delete()
+          }, 100)
+
+        let suggestembed = new Discord.MessageEmbed()
+        .setTitle('**__Protected Message__**')
+        .setDescription(`${suggestmessage}`)
+        .setColor("FF6F52")
+        
+        channel.send({embeds: [suggestembed]});
+    }
+};
