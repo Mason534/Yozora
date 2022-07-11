ae4745295f1951d567a9280fb74d7b0e5c13e5ee
Mason534 <masonmiller534@gmail.com>
2022-07-11 16:47:46 -0500

    Update life-invader.js

--- Commands/RP/life-invader.js
+++ Commands/RP/life-invader.js
@@ -6,22 +6,28 @@ module.exports = {
     permissions: 'SEND_MESSAGES',
     description: "posts a life invader embed",
     cooldown: 3,
-    execute(message, args, commandName, client, Discord) {
+    async execute(message, args, commandName, client, Discord) {
+
+        const channel = message.guild.channels.cache.get('693829458431574066');
+
         const LF = (args.slice('').join(" "))
         setTimeout(function() {
             message.delete()
-          }, 1000)
-        const User = message.author.username
+          }, 500)
+        
         const Response = new MessageEmbed()
         .setTitle(`New Life-Invader Post!`)
         .setAuthor(message.author.username, message.author.avatarURL({dynamic: true}))
         .setColor('RED')
-	    .setDescription(`${message.author}: ${LF}`)
-	    .setThumbnail('http://cdn.themis-media.com/media/global/images/library/deriv/595/595961.jpg')
-	    .setFooter(`Life-Invader, it's tech, it's your life!`)
-	    .setTimestamp(message.createdTimestamp)
+	      .setDescription(`${message.author}: ${LF}`)
+	      .setThumbnail('http://cdn.themis-media.com/media/global/images/library/deriv/595/595961.jpg')
+	      .setFooter(`Life-Invader, it's tech, it's your life!`)
+	      .setTimestamp(message.createdTimestamp)
         
         
-        message.channel.send({embeds: [Response]})
+        let msg = await channel.send({embeds: [Response]});
+
+        msg.react("👍");
+        msg.react("👎");
 
     }}
